f33c5144ea4216a00c285e4fe394a0dd843ddae7
G Manjunth <manjudba1222@gmail.com>
2025-01-02 14:16:21 +0530

    hi

---
+++ color.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e [31m"
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then 
+echo -e "$2..$R succussful"
+else 
+echo "$2 Failed"
+exist 1
+fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+
+echo " User not having sufficent permissions"
+exist 1
+else
+ dnf list installed |grep mysql
+
+ if [ $? -eq 0 ]
+then 
+echo -e "$R Already Installed MYSQL"
+else
+dnf install mysql-server -y
+VALIDATE $? "mysql"
+fi
+fi
+
+
