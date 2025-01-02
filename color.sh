2542860b7ad846878a9089211a8e462c8fb91cd0
G Manjunth <manjudba1222@gmail.com>
2025-01-02 15:53:30 +0530

    hi

--- color.sh
+++ color.sh
@@ -4,13 +4,15 @@ USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
-
+PATH="/var/log/shell-script"
+FILE="log.txt"
+FULLPATH="$PATH/$FILE"
 VALIDATE(){
     if [ $1 -eq 0 ]
 then 
-echo -e "$G $2..succussful"
+echo -e "$G $2..succussful" &>$FULLPATH
 else 
-echo -e "$R $2 Failed"
+echo -e "$R $2 Failed"  &>$FULLPATH
 exist 1
 fi
 }
@@ -18,14 +20,14 @@ fi
 if [ $USERID -ne 0 ]
 then 
 
-echo " User not having sufficent permissions"
+echo " User not having sufficent permissions"  &>$FULLPATH
 exist 1
 else
  dnf list installed |grep mysql
 
  if [ $? -eq 0 ]
 then 
-echo -e "$G Already Installed MYSQL"
+echo -e "$G Already Installed MYSQL"  &>$FULLPATH
 else
 dnf install mysql-server -y
 VALIDATE $? "mysql"
